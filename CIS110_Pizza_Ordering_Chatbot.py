13af9308ae26c6affbde8cf1d0242dcb1610c814
username <useremail>
2025-12-19 00:58:15 -0800

    Added Nested Loops

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,49 +1,57 @@
-from multiprocessing.connection import deliver_challenge
-
-
 userName = input("\nEnter your name:  ")
 while len(userName) == 0:
     userName = input("Name cannot be blank! Please enter your name: ")
-if userName.lower()== "ffergiesunny22":
+if userName.lower() == "ffergiesunny22":
     print(f"\Hello,{userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Nice to meet you!")
-name = input("\nEnter your name: ")
-print(f"\nHello, ffergiesunny22. Nice to meet you")
-size = input("\nWhat size do you want? Enter small, medium, or large:  ")
-while size.lower() not in ["small", "medium", "large"]:
-    size = input("Invaild value: Please enter small, medium, or large:  ")
-flavor = input("\nEnter the flavor of pizza:  ")
-while len(size) == 0:
-   flavor = input("Flavor cannot be blank:  Please enter a flavor:   ")
-crustType = input("\nWhat type of crust do you want:  ")
-while len(crustType) == 0:
-    crustType =input("Crust type cannot be blank: Please enter crust type:   ")
-quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
-while not quantity.isdigit():
-    quantity = input("\nValue not recongized. Please enter a numeric value:   ")
-quantity =int(quantity)
-method = input("\nIs this carry out or delivery:  ")
-while method not in ["carry out", "delivery"]:
-    method =  input("Invaild value! Please enter carry out or delivery:   ")
-if method.lower() == "delivery":
-    deliveryFee = 5
-else: 
-    deliveryFee = 0
-salesTax = 1.1
-if size.lower() == "small":
-    pizzaCost = 8.99
-elif size.lower() == "medium":
-    pizzaCost = 14.99
-elif size.lower() == "large":
-    pizzaCost = 17.99
-total = quantity * pizzaCost * salesTax + deliveryFee
+keepGoing = "y"
+while keepGoing.lower() == "y":
+    size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+    while size.lower() not in ["small", "medium", "large"]:
+        size = input("Invaild value: Please enter small, medium, or large:  ")
+    flavor = input("\nEnter the flavor of pizza:  ")
+    while len(size) == 0:
+       flavor = input("Flavor cannot be blank:  Please enter a flavor:   ")
+    crustType = input("\nWhat type of crust do you want:  ")
+    while len(crustType) == 0:
+        crustType =input("Crust type cannot be blank: Please enter crust type:   ")
+    quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+    while not quantity.isdigit():
+        quantity = input("\nValue not recongized. Please enter a numeric value:   ")
+    quantity =int(quantity)
+    method = input("\nIs this carry out or delivery:  ")
+    while method not in ["carry out", "delivery"]:
+        method =  input("Invaild value! Please enter carry out or delivery:   ")
+    if method.lower() == "delivery":
+        deliveryFee = 5
+    else: 
+        deliveryFee = 0
+    salesTax = 1.1
+    if size.lower() == "small":
+        pizzaCost = 8.99
+    elif size.lower() == "medium":
+        pizzaCost = 14.99
+    elif size.lower() == "large":
+        pizzaCost = 17.99
+    total = quantity * pizzaCost * salesTax + deliveryFee
 
-print("*" * 10)  # Line 15, fixed
-print(f"Thank you, {name}, for your order.")
-print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:.2f}")  # Line 18, fixed
-if total >= 50:
-    print("\nCongratulations! You've been awarded a $10 Off coupon for your next order.")
-else:
-    print("\nOrder over $50 will receive a free $10 Off coupon!")
-print("*" * 10)
+    print("*" * 10)  # Line 15, fixed
+    print(f"Thank you, {userName}, for your order.")
+    print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:.2f}")  # Line 18, fixed
+    if total >= 50:
+        print("\nCongratulations! You've been awarded a $10 Off coupon for your next order.")
+    else:
+        print("\nOrder over $50 will receive a free $10 Off coupon!")
+    print("*" * 10)
+    print("Order has been received. ETA 3 mins!")
+    for min in range(3, 0, -1):
+        print(min, "minutes remaining")
+        for seconds in range(60, 0, -1):
+            print(seconds, end = "\r")
+            import time
+            time.sleep(1)
+    print("Order is ready!")
+    keepGoing = input("Do you want to place another order? Enter y or n:  ")
+    while keepGoing.lower() not in ["y","n"]:
+        keepGoing = input("Invalid value: Enter y or n:")
